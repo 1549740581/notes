87bafc9cd177f84be11c2fd2fff2f477e0e76d3c
Sherman2571 <sherman@mail.nwpu.edu.cn>
2019-12-20 13:21:51 +0800

    模拟消息中间件

---
+++ spring_annotation/src/main/java/nwpu/sherman/servlet/DeferredResultQueue.java
@@ -0,0 +1,23 @@
+package nwpu.sherman.servlet;
+
+import org.springframework.web.context.request.async.DeferredResult;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ * 模拟消息中间件
+ *
+ * @author sherman
+ */
+public class DeferredResultQueue {
+    private static Queue<DeferredResult<Object>> queue = new ConcurrentLinkedQueue<>();
+
+    public static void  save(DeferredResult<Object> deferredResult) {
+        queue.add(deferredResult);
+    }
+
+    public static DeferredResult<Object> get() {
+        return queue.poll();
+    }
+}
