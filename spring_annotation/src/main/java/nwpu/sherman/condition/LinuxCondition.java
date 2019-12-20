2dd07c0593c6d5f213b96e1f30a6112027aba184
Sherman2571 <sherman@mail.nwpu.edu.cn>
2019-12-20 13:12:02 +0800

    演示@Conditional注解

---
+++ spring_annotation/src/main/java/nwpu/sherman/condition/LinuxCondition.java
@@ -0,0 +1,19 @@
+package nwpu.sherman.condition;
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+/**
+ * 演示@Conditional注解
+ *
+ * Linux操作系统的判断条件
+ *
+ * @author sherman
+ */
+public class LinuxCondition implements Condition {
+    @Override
+    public boolean matches(ConditionContext conditionContext, AnnotatedTypeMetadata annotatedTypeMetadata) {
+        return conditionContext.getEnvironment().getProperty("os.name").contains("Linux");
+    }
+}
