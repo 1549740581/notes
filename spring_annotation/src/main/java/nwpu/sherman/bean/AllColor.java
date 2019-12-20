34a677379ee49633a7953b11733d6c4e415df2e7
Sherman2571 <sherman@mail.nwpu.edu.cn>
2019-12-20 13:10:51 +0800

    @Import & ImportBeanDefinitionRegistrar

---
+++ spring_annotation/src/main/java/nwpu/sherman/bean/AllColor.java
@@ -0,0 +1,12 @@
+package nwpu.sherman.bean;
+
+/**
+ * 演示@Import & ImportBeanDefinitionRegistrar
+ *
+ * 在运行期间检查当前IOC容器中是否有Red、Green、Blue的Bean，如果有，
+ * 则将该Bean对象注册到IOC容器中
+ *
+ * @author sherman
+ */
+public class AllColor {
+}
