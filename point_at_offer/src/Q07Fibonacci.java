5fe31cadfe07359ed428b218d908cab55b68fbd7
Sherman2571 <sherman@mail.nwpu.edu.cn>
2019-12-21 16:40:11 +0800

    07 斐波那契数列的第n项

---
+++ point_at_offer/src/Q07Fibonacci.java
@@ -0,0 +1,21 @@
+/**
+ * 斐波那契数列的第n项，第0项为0，第一项为1
+ *
+ * @author sherman
+ */
+public class Q07Fibonacci {
+    public int fibonacci(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        int fo = 0;
+        int f1 = 1;
+        int fn = 0;
+        while (n-- > 1) {
+            fn = fo + f1;
+            fo = f1;
+            f1 = fn;
+        }
+        return fn;
+    }
+}
