c7a2b2b7ad3f51104330415974f41f092836449b
Sherman2571 <sherman@mail.nwpu.edu.cn>
2019-12-26 23:19:07 +0800

    单链表节点定义

---
+++ play_with_algs/src/utils/ListNode.java
@@ -0,0 +1,15 @@
+package utils;
+
+/**
+ * 单链表节点定义
+ *
+ * @author sherman
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
