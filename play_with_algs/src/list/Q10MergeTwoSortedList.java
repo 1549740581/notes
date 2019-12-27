736c24c8ef72787023b1c0366d34d9503cfe56cd
Sherman2571 <sherman@mail.nwpu.edu.cn>
2019-12-27 16:12:41 +0800

    21 merge two sorted list

---
+++ play_with_algs/src/list/Q10MergeTwoSortedList.java
@@ -0,0 +1,57 @@
+package list;
+
+import utils.ListNode;
+
+/**
+ * 21 merge two sorted list
+ *
+ * @author sherman
+ */
+public class Q10MergeTwoSortedList {
+    /**
+     * 递归
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode head = null;
+        if (l1.val < l2.val) {
+            head = l1;
+            head.next = mergeTwoLists(l1.next, l2);
+        } else {
+            head = l2;
+            head.next = mergeTwoLists(l1, l2.next);
+        }
+        return head;
+    }
+
+    /**
+     * 非递归
+     */
+    public ListNode mergeTwoLists01(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        if (l1 != null) {
+            cur.next = l1;
+        }
+        if (l2 != null) {
+            cur.next = l2;
+        }
+        return dummy.next;
+    }
+
+}
