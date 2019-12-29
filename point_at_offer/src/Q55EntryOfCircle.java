c23983a8772627508b4ed05eeb23c73f08a678a7
tanglei302wqy <tanglei2571@163.com>
2019-12-29 13:56:46 +0800

    55 链表中环的入口节点

---
+++ point_at_offer/src/Q55EntryOfCircle.java
@@ -0,0 +1,36 @@
+/**
+ * 链表中环的入口节点
+ * 给一个链表，若其中包含环，请找出该链表的环的入口节点，否则返回null。
+ *
+ * @author sherman
+ */
+public class Q55EntryOfCircle {
+    public ListNode entryNodeOfLoop(ListNode pHead) {
+        if (pHead == null || pHead.next == null) {
+            return null;
+        }
+        ListNode slow = pHead;
+        ListNode fast = pHead;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast)
+                break;
+        }
+        // 没有环
+        if (fast == null) {
+            return null;
+        } else {
+            /**
+             * 寻找环
+             *
+             */
+            slow = pHead;
+            while (slow != fast) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+        }
+        return slow;
+    }
+}
