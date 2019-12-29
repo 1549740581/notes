8a17fc7d26fd3130a5286c09b46c127b64828221
tanglei302wqy <tanglei2571@163.com>
2019-12-29 13:24:01 +0800

    49 把字符串转换成整数

---
+++ point_at_offer/src/Q49StringToInt.java
@@ -0,0 +1,37 @@
+/**
+ * 把字符串转换成整数
+ * 将一个字符串转换成一个整数，要求不能使用字符转换成整数的库函数。数值0或者字符串不是一个合法的数值都会返回0
+ *
+ * @author sherman
+ */
+public class Q49StringToInt {
+    public int strToInt(String str) {
+        if (str == null || "".equals(str.trim()) || !isValid(str)) {
+            return 0;
+        }
+        String trim = str.trim();
+        char first = str.charAt(0);
+        int idx = 0;
+        int sign = 1;
+        if (!Character.isDigit(first)) {
+            ++idx;
+            if (first == '-') {
+                sign = -1;
+            }
+        }
+        char[] charArray = trim.substring(idx).toCharArray();
+        long res = 0;
+        for (char c : charArray) {
+            res = 10 * res + (c - '0');
+        }
+        res *= sign;
+        if (res < Integer.MIN_VALUE || res > Integer.MAX_VALUE) {
+            return 0;
+        }
+        return (int) res;
+    }
+
+    private boolean isValid(String str) {
+        return str.trim().matches("[+-]?\\d+");
+    }
+}
