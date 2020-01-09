164345d4f26ecd970abf55004db6306f8da2767e
tanglei302wqy <tanglei2571@163.com>
2020-01-09 21:54:23 +0800

    TreeNode定义

---
+++ play_with_algs/src/utils/TreeNode.java
@@ -0,0 +1,14 @@
+package utils;
+
+/**
+ * @author sherman
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
