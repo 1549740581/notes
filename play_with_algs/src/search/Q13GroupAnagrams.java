3526461de6124d4336575d0070c66817360713f8
tanglei302wqy <tanglei2571@163.com>
2020-01-10 02:07:56 +0800

    49 group anagrams

---
+++ play_with_algs/src/search/Q13GroupAnagrams.java
@@ -0,0 +1,50 @@
+package search;
+
+import java.util.*;
+
+/**
+ * 49 group anagrams
+ * 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+ *
+ * @author sherman
+ */
+public class Q13GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> maps = new HashMap<>();
+        for (String str : strs) {
+            String key = sortString(str);
+            List<String> list = maps.get(key);
+            if (list == null) {
+                List<String> tmp = new ArrayList<>();
+                tmp.add(str);
+                maps.put(key, tmp);
+            } else {
+                list.add(str);
+            }
+        }
+        List<List<String>> res = new LinkedList<>();
+        for (Map.Entry<String, List<String>> entry : maps.entrySet()) {
+            res.add(entry.getValue());
+        }
+        return res;
+    }
+
+    private String sortString(String str) {
+        char[] chars = str.toCharArray();
+        Arrays.sort(chars);
+        StringBuilder sb = new StringBuilder();
+        for (char ch : chars) {
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> lists = new Q13GroupAnagrams().groupAnagrams(strs);
+        for (List<String> list : lists) {
+            list.forEach(System.out::println);
+            System.out.println("========");
+        }
+    }
+}
