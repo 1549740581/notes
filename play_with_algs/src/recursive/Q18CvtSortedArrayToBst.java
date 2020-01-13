dce98b2588c3a89a6b42b8f62fae91f1fbb4d68b
tanglei302wqy <tanglei2571@163.com>
2020-01-13 20:52:34 +0800

    108 convert sorted array to binary search tree

---
+++ play_with_algs/src/recursive/Q18CvtSortedArrayToBst.java
@@ -0,0 +1,25 @@
+package recursive;
+
+import utils.TreeNode;
+
+/**
+ * 108 convert sorted array to binary search tree
+ *
+ * @author sherman
+ */
+public class Q18CvtSortedArrayToBst {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return sortedArrayToBST(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode sortedArrayToBST(int[] nums, int lo, int hi) {
+        if (lo > hi) {
+            return null;
+        }
+        int mid = lo + ((hi - lo) >> 1);
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = sortedArrayToBST(nums, lo, mid - 1);
+        root.right = sortedArrayToBST(nums, mid + 1, hi);
+        return root;
+    }
+}
