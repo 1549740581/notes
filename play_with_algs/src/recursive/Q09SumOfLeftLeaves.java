b6a8545a92cc37974cba7bc67af882432fad530d
tanglei302wqy <tanglei2571@163.com>
2020-01-13 20:48:22 +0800

    404 sum of left leaves

---
+++ play_with_algs/src/recursive/Q09SumOfLeftLeaves.java
@@ -0,0 +1,22 @@
+package recursive;
+
+import utils.TreeNode;
+
+/**
+ * 404 sum of left leaves
+ *
+ * @author sherman
+ */
+public class Q09SumOfLeftLeaves {
+    public int sumOfLeftLeaves(TreeNode root) {
+        int cnt = 0;
+        if (root != null) {
+            if (root.left != null && root.left.left == null && root.left.right == null) {
+                cnt += root.left.val;
+            }
+            cnt += sumOfLeftLeaves(root.left);
+            cnt += sumOfLeftLeaves(root.right);
+        }
+        return cnt;
+    }
+}
