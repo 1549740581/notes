c04b245859a09ba50f2cbcd5b62ca86ff2135c5d
tanglei302wqy <tanglei2571@163.com>
2020-01-13 20:52:45 +0800

    230 kth smallest element in a bst

---
+++ play_with_algs/src/recursive/Q19KthSmallestElemInBst.java
@@ -0,0 +1,32 @@
+package recursive;
+
+import utils.TreeNode;
+
+/**
+ * 230 kth smallest element in a bst
+ * 给定一个二叉搜索树，编写一个函数kthSmallest 来查找其中第k个最小的元素。
+ *
+ * @author sherman
+ */
+public class Q19KthSmallestElemInBst {
+    public int kthSmallest(TreeNode root, int k) {
+        int leftCnt = countNode(root.left);
+        if (leftCnt + 1 == k) {
+            return root.val;
+        } else if (k <= leftCnt) {
+            return kthSmallest(root.left, k);
+        } else {
+            /**
+             * 减去左子树 + 当前子树根节点
+             */
+            return kthSmallest(root.right, k - leftCnt - 1);
+        }
+    }
+
+    private int countNode(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return countNode(node.left) + countNode(node.right) + 1;
+    }
+}
