347b4ad0c2de1561bc6eb433e9e6c8d5f67ac1bb
tanglei302wqy <tanglei2571@163.com>
2020-01-13 20:48:31 +0800

    257 binary tree paths

---
+++ play_with_algs/src/recursive/Q10BiTreePath.java
@@ -0,0 +1,40 @@
+package recursive;
+
+import utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 257 binary tree paths
+ * 给定一个二叉树，返回所有从根节点到叶子节点的路径。
+ *
+ * @author sherman
+ */
+public class Q10BiTreePath {
+    private List<String> res = new LinkedList<>();
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        if (root == null) {
+            return res;
+        }
+        return binaryTreePaths(root, new ArrayList<>());
+    }
+
+    private List<String> binaryTreePaths(TreeNode root, ArrayList<String> tmp) {
+        tmp.add(String.valueOf(root.val));
+        if (root.left == null && root.right == null) {
+            CharSequence[] cs = new CharSequence[tmp.size()];
+            res.add(String.join("->", tmp.toArray(cs)));
+        }
+        if (root.left != null) {
+            binaryTreePaths(root.left, tmp);
+        }
+        if (root.right != null) {
+            binaryTreePaths(root.right, tmp);
+        }
+        tmp.remove(tmp.size() - 1);
+        return res;
+    }
+}
