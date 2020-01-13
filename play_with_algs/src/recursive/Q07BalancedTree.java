8fe35d036505b6b0eef08d0e32902fedde6f81c9
tanglei302wqy <tanglei2571@163.com>
2020-01-13 20:48:01 +0800

    110 balanced binary tree

---
+++ play_with_algs/src/recursive/Q07BalancedTree.java
@@ -0,0 +1,29 @@
+package recursive;
+
+import utils.TreeNode;
+
+/**
+ * 110 balanced binary tree
+ *
+ * @author sherman
+ */
+public class Q07BalancedTree {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        int leftDepth = getDepth(root.left);
+        int rightDepth = getDepth(root.right);
+        if (Math.abs(leftDepth - rightDepth) > 1) {
+            return false;
+        }
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int getDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(getDepth(root.left), getDepth((root.right))) + 1;
+    }
+}
