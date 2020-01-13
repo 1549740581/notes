d239e8398e3c658fdd156e8c3f1113d1b494f036
tanglei302wqy <tanglei2571@163.com>
2020-01-13 20:52:06 +0800

    98 validate binary search tree

---
+++ play_with_algs/src/recursive/Q16ValidBst.java
@@ -0,0 +1,27 @@
+package recursive;
+
+import utils.TreeNode;
+
+/**
+ * 98 validate binary search tree
+ * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+ *
+ * @author sherman
+ */
+public class Q16ValidBst {
+    private long pre = Long.MIN_VALUE;
+
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        boolean leftCond = isValidBST(root.left);
+        boolean midCond = false;
+        if (root.val > pre) {
+            midCond = true;
+            pre = root.val;
+        }
+        boolean rightCond = isValidBST(root.right);
+        return leftCond && midCond && rightCond;
+    }
+}
