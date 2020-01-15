9d968ed8a0f020946b706d9ab92a493207017e38
tanglei302wqy <tanglei2571@163.com>
2020-01-15 16:04:03 +0800

    198 house robber I

---
+++ play_with_algs/src/dp/Q09HouseRobberI.java
@@ -0,0 +1,35 @@
+package dp;
+
+/**
+ * 198 house robber I
+ * 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，
+ * 如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。给定一个代表每个房屋存放金额的非负整数数组，计算你在不触动警报装置的情况下，
+ * 能够偷窃到的最高金额。
+ * 输入: [2,7,9,3,1]
+ * 输出: 12
+ *
+ * @author sherman
+ */
+public class Q09HouseRobberI {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        /**
+         * dp[i] == j：偷房子i能够得到最大价值为j
+         */
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[nums.length - 1];
+    }
+}
