d5fb78206abcd6cd934438f5d0a6d0a796c6668f
TangLei <59001156+tanglei302wqy@users.noreply.github.com>
2020-01-26 16:33:20 +0800

    111 minimum depth of binary tree（增加层序遍历解法）

--- play_with_algs/src/recursive/Q02MinDepthOfBiTree.java
+++ play_with_algs/src/recursive/Q02MinDepthOfBiTree.java
@@ -1,6 +1,7 @@
 package recursive;
 
 import utils.TreeNode;
+import java.util.*;
 
 /**
  * 111 minimum depth of binary tree
@@ -23,5 +24,37 @@ public class Q02MinDepthOfBiTree {
          * 可以用left + right + 1统一表示。
          */
         return (left != 0 && right != 0) ? Math.min(left, right) + 1 : left + right + 1;
+    }
+    
+    /**
+     * 层序遍历
+     */
+    public int minDepthLevelTraversal(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        int level = 1;
+        queue.add(root);
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; ++i) {
+                TreeNode node = queue.poll();
+                if (node.left == null && node.right == null) {
+                    return level;
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            ++level;
+        }
+        throw new RuntimeException();
     }
 }
