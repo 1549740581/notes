cc411dabcfde2042c01c84c060952882aeb99568
tanglei302wqy <tanglei2571@163.com>
2020-02-22 04:32:36 +0800

    修复一个超链接错误

--- 玩转算法面试/src/recursive/Q09BiTreePath.java
+++ 玩转算法面试/src/recursive/Q09BiTreePath.java
@@ -12,7 +12,7 @@ import java.util.List;
  *
  * @author sherman
  */
-public class Q9BiTreePath {
+public class Q09BiTreePath {
     private List<String> res = new LinkedList<>();
 
     public List<String> binaryTreePaths(TreeNode root) {
